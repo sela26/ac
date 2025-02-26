c0d2a6ee0bddbc432c543feb4e8c5e1ca4e1d85d
sela26 <sela.schulze@stud.leuphana.de>
2025-02-26 12:06:48 +0100

    Update get_active.py
    comments

--- get_active.py
+++ get_active.py
@@ -4,6 +4,7 @@ from streamlit_theme import \
 
 
 def get_active_page():
+    # title banner, light / dark mode
     theme = st_theme()
     if theme["base"] == "light":
         st.image("images/light_get_active.png")
@@ -14,7 +15,10 @@ def get_active_page():
     st.subheader("Get active with DON-AID and Active Cycle!")
     st.divider()
     st.subheader("Join our fight against period poverty.")
+    
     c1, c2 = st.columns(2)
+    
+    # info text
     with c1:
         st.write("**1 in 4 teenagers and 1 in 3 adults in the United States cannot afford "
                  "menstrual products**, affecting especially teens of color and "
@@ -28,28 +32,38 @@ def get_active_page():
                  "and with dignity. Period poverty isn’t just about missing products—it’s about "
                  "missed opportunities, health risks, and the emotional toll of navigating a world "
                  "where basic needs are out of reach.")
+        
+    # image, illustration by Milica    
     with c2:
         st.image("images/TB2-period-poverty.jpeg")
 
+    # link for more info
     st.markdown(
         "[CLICK HERE to learn about absence of girls in school, due to LACK OF MENSTRUAL EDUCATION!]"
         "(https://blogs.worldbank.org/en/education/globally-periods-are-causing-girls-be-absent-school)")
+    
     st.subheader("Together, we can make a difference.")
     st.write("For many, the added cost of menstrual products makes an already tight situation even harder. "
              "Period poverty isn’t just someone else’s problem—it’s a challenge "
              "we all face together.")
+
     st.subheader("Cooperation with DON-AID and Active Cycle")
+    # text about Sela + Milica and our ideas
     st.write(
         "**Sela Schulze and Milica Stanojić** are both young founders "
         "who engage in the **fight for women’s rights**. “Active Cycle“ helps humans with uteruses to "
         "understand their menstrual cycle and balance their lifestyle around it. “DON-AID“ "
         "is a donation platform that specifically supports women in war zones and shares testimonials to raise awareness.")
+
+    # about active cycle
     st.write(
         "**Active Cycle**'s mission is to reduce the stigma surrounding menstrual health "
         "by providing guidance and support for cycle-syncing workout routines at all levels. "
         "We show that the menstrual cycle is not a weakness but a source of strength. Use the "
         "app to educate yourself on your menstrual cycle and improve your life quality, by means "
         "of suggestions and cycle-tracking.")
+
+    # about don-aid
     st.write(
         "**DON-AID** actively shares your donations and improves sanitary access through money, material donations and "
         "signatures. As more than **1.5 billion people still lack basic basic sanitation services and 1 in 10 women in "
@@ -60,6 +74,8 @@ def get_active_page():
     st.write(
         "With DON-AID every donation can make a change. Comment on personal stories of women in war zones, fund "
         "teaching possibilities in war zones and donate sanitary products.")
+
+    # link to don-aid
     st.markdown(
         "[Try DON-AID]"
         "(https://don-aid.streamlit.app/)")
